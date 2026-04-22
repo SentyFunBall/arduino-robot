2516253c9974846e0c47b83d2b0480d82896ef44
SentyFunball <haydenbugatti@gmail.com>
2026-04-21 21:51:42 -0700

    add liquid crystal support

--- robot/robot.ino
+++ robot/robot.ino
@@ -4,15 +4,19 @@
 #include "led.h"
 #include "button.h"
 
+#include "LiquidCrystal.h"
+
 Ultrasonic sensor(9, 10);
 Motor moto(4, 3, 2);
-LED led(21);
-Button btn(20);
+LED led(11);
+Button btn(12);
+LiquidCrystal lcd(22, 23, 24, 25, 26, 27);
 
 void setup() {
   U0init(9600);
   moto.off();
   moto.setDirection(1);
+  lcd.begin(16, 2);
 }
 
 void loop() {
@@ -24,11 +28,33 @@ void loop() {
   U0putstrln(floatToUChar(float(speed), 0));
   //moto.setSpeed(speed);
   delay(100);*/
-  if (btn.isPressedPulse()) {
+  /*if (btn.isPressedPulse()) {
     led.Toggle();
     //U0putstrln("pressed");
   }
-  delay(10);
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  if (btn.isPressed()) {
+    lcd.print("pressed");
+  } else {
+    lcd.print("not lol");
+  }*/
+
+  float d = sensor.getDistance();
+  unsigned char* dstr = floatToUChar(d, 2);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Dist: ");
+  lcd.print((char*)dstr);
+  lcd.print("mm");
+  if (d < 60) {
+    led.On();
+  } else {
+    led.Off();
+  }
+
+  delay(100);
 }
 
 unsigned char* floatToUChar(float value, int decimals) {
